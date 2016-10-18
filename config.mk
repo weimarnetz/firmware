0cddaf560f1402d6c87425f1e2cfa979d85049db
mt <martin.tippmann@gmail.com>
2016-10-18 12:44:21 +0200

    deployment settings

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
 TARGET=ar71xx
-PACKAGES_LIST_DEFAULT=default devel
+PACKAGES_LIST_DEFAULT=default
 LEDE_SRC=git://git.lede-project.org/source.git
 LEDE_COMMIT=HEAD
 MAKE_ARGS=
