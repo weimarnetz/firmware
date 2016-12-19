1d74d2a222a41c1e8975ba5b40d79802582cd007
glycoknob <mt@i3o.de>
2016-12-19 15:48:42 +0100

    fix build

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
 TARGET=ar71xx
-PACKAGES_LIST_DEFAULT=bratwurst
+PACKAGES_LIST_DEFAULT="default devel"
 LEDE_SRC=git://git.lede-project.org/source.git
 LEDE_COMMIT=HEAD
 MAKE_ARGS=
