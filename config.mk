b82bf030f81c7a0933747a9c01d35657c231114d
glycoknob <mt@i3o.de>
2017-07-28 03:19:54 +0200

    trying to figure out why multiple package sets are not build

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
 TARGET=ar71xx
-PACKAGES_LIST_DEFAULT="weimarnetz-next weimarnetz-ffwizard"
+PACKAGES_LIST_DEFAULT=weimarnetz-next weimarnetz-gebranntemandeln
 LEDE_SRC=git://git.lede-project.org/source.git
 LEDE_COMMIT=HEAD
 MAKE_ARGS=
