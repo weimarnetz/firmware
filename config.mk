666f7cc9f0f7d573700fe7f8663260e6c9f2db5d
mt <mt@i3o.de>
2017-10-02 00:05:58 +0200

    add camburg

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
 TARGET=ar71xx
-PACKAGES_LIST_DEFAULT=weimarnetz-next weimarnetz
+PACKAGES_LIST_DEFAULT=weimarnetz camburg
 LEDE_SRC=git://git.lede-project.org/source.git
 LEDE_COMMIT=HEAD
 MAKE_ARGS=
