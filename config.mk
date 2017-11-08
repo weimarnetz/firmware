b90fe31987da48bab3905e937c03752a3774e7e4
glycoknob <mt@i3o.de>
2017-11-08 09:54:46 +0100

    fix feeds.conf

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
 TARGET=ar71xx
-PKG_LISTS=weimarnetz camburg
+PKG_LISTS=weimarnetz-gebranntemandeln
 LEDE_SRC=git://git.lede-project.org/source.git
 LEDE_COMMIT=lede-17.01
 MAKE_ARGS=
