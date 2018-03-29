c9dcf457e191bd70aea1836cdc0255be0039f7d7
mt <mt@i3o.de>
2018-03-29 04:01:56 +0200

    no separate camburg profile

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
 TARGET=ar71xx
-PKG_LIST=weimarnetz camburg
+PKG_LIST=weimarnetz
 OPENWRT_SRC=git://git.lede-project.org/source.git
 OPENWRT_COMMIT=HEAD
 MAKE_ARGS=
