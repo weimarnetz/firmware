8bb1c055513c5ece9e8d08e67a763f6bf2898eaa
Martin Tippmann <martin.tippmann@gmail.com>
2019-01-19 21:43:10 +0100

    pin to specific commit

--- config.mk
+++ config.mk
@@ -2,6 +2,6 @@
 SHELL:=$(shell which bash)
 TARGET=ar71xx
 PKG_LIST=weimarnetz
-OPENWRT_SRC=git://git.lede-project.org/source.git
-OPENWRT_COMMIT=HEAD
+OPENWRT_SRC=git://git.openwrt.org/openwrt/openwrt.git
+OPENWRT_COMMIT=7f98cd8d503e33ad43d6273d417474086bcb446d
 MAKE_ARGS=
