bc65d57a004bb3c34e0f12f3c9a420da47e1a2fa
Martin Tippmann <martin.tippmann@gmail.com>
2019-04-09 22:12:04 +0200

    bump openwrt 18.06 to current commit

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx
 PKG_LIST=weimarnetz
 OPENWRT_SRC=git://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=v18.06.2
+OPENWRT_COMMIT=c8a8294f6e64367b7dc92eda6edca28efbb82053
 MAKE_ARGS=
