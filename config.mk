392d79b441546033d1203c685855d3d9bf61e5bf
Martin Tippmann <martin.tippmann@gmail.com>
2019-06-01 11:25:21 +0200

    fix master build, remove uci, add owsd - fix mir3g

--- config.mk
+++ config.mk
@@ -2,6 +2,6 @@
 SHELL:=$(shell which bash)
 TARGET=ar71xx
 PKG_LIST=weimarnetz
-OPENWRT_SRC=git://git.lede-project.org/source.git
+OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
 OPENWRT_COMMIT=HEAD
 MAKE_ARGS=
