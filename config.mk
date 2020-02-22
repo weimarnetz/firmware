352a92ce25b9f6bbab17c9cdaf708a51f6051d9d
Martin Tippmann <martin.tippmann@gmail.com>
2020-02-22 22:32:02 +0100

    update openwrt, add ath79

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx
 PKG_LIST=weimarnetz
 OPENWRT_SRC=git://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=ae953449f2f6807174a2c000b9fc5a31b46f1798
+OPENWRT_COMMIT=2d3a93335ae4ea9ca63e4bae798738b5befca440
 MAKE_ARGS=
