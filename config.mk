38b6e028f2f143d64b56d85cebf7ee152f4d9a3f
Martin Tippmann <martin.tippmann@gmail.com>
2020-03-19 00:57:15 +0100

    ath79 support

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx
 PKG_LIST=weimarnetz
 OPENWRT_SRC=git://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=2d3a93335ae4ea9ca63e4bae798738b5befca440
+OPENWRT_COMMIT=74a8e36975a7148b4a901b849facbb6fa309ea97
 MAKE_ARGS=
