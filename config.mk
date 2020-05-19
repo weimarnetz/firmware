89dab44ee9a725ff08ddfddaeb0712aa402a9334
Martin Tippmann <tippmann@edu-sharing.com>
2020-05-19 20:04:12 +0200

    bump to latest openwrt - add mir3g again

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx
 PKG_LIST=weimarnetz
 OPENWRT_SRC=git://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=631c437a91c20df678b25dcc34fe23636116a35a
+OPENWRT_COMMIT=5f826919232db2ca2d3d7e9da00de93c5f6a95bf
 MAKE_ARGS=
