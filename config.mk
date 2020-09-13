cdaddbe49f00af4176abc2e45bf3d39c8d6913e1
Martin Tippmann <tippmann@edu-sharing.com>
2020-09-13 16:04:13 +0200

    test build for openwrt with 4mb ath79_tiny devices on master

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx
 PKG_LIST=weimarnetz
 OPENWRT_SRC=git://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=f3f38f40da74a193b4177be8d0fa746d00a753ea
+OPENWRT_COMMIT=master
 MAKE_ARGS=
